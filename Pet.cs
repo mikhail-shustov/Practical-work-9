528cd8d2da4226cb5d0c5d4c97d6ef52ed36bdae
mikhail-shustov <mikhail.shustov08@gmail.com>
2025-10-22 12:47:49 +0300

    Add files via upload

---
+++ Pet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet
+{
+    internal class Pet
+    {
+        internal class pet
+        {
+            private string name;
+            private int energy;
+            public string Name
+            {
+                get => name;
+                set => name = value;
+            }
+
+            public int Energy
+            {
+                get => energy;
+                set
+                {
+                    if (value < 0)
+                        energy = 0;
+                    else if (value > 100)
+                        energy = 100;
+                    else
+                        energy = value;
+                }
+            }
+            public pet(string name, int energy)
+            {
+                Name = name;
+                Energy = energy;
+            }
+            public void Play()
+            {
+                Energy -= 20;
+                Console.WriteLine($"{Name} играет, энергия: {Energy}.");
+            }
+            public void Rest()
+            {
+                Energy += 30;
+                Console.WriteLine($"{Name} отдыхает, энергия: {Energy}.");
+            }
+        }
+    }
+}
