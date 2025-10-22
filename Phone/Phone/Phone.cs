f4d99fd4459acaa2413f53656273d611f3a5a17c
mikhail-shustov <mikhail.shustov08@gmail.com>
2025-10-22 11:44:37 +0300

    Add files via upload

---
+++ Phone/Phone/Phone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    internal class Phone
+    {
+        private string brand;
+        private int batteryLevel;
+        public string Brand
+        {
+            get { return brand; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Ошибка");
+                }
+                else
+                {
+                    brand = value;
+                }
+            }
+        }
+        public int BatteryLevel
+        {
+            get { return batteryLevel; }
+            set
+            {
+                if (value < 0 && value > 100)
+                {
+                    Console.WriteLine("Ошибка");
+                }
+                else
+                {
+                    batteryLevel = value;
+                }
+            }
+        }
+        public Phone(string brand, int batteryLevel)
+        {
+            Brand = brand;
+            BatteryLevel = batteryLevel;
+        }
+        public void UsePhone()
+        {
+            int Level = batteryLevel - 10;
+            Console.WriteLine($"Телефон {brand}, заряд: {Level}%");
+        }
+    }
+}
---
+++ Phone/Phone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new Phone("iPhone", 50);
+                phone.UsePhone();
+        }
+    }
+}
