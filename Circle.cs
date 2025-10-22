6085d96f384339ce924af1a3f55afcc6f6fb5dba
mikhail-shustov <mikhail.shustov08@gmail.com>
2025-10-22 12:45:50 +0300

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Circle
+    {
+        private double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value > 0)
+                {
+                    radius = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Радиус должен быть больше 0");
+                }
+            }
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public void GetArea()
+        {
+            double area = Math.PI * radius * radius;
+            Console.WriteLine($"Площадь круга: {area:F0}");
+        }
+    }
+}
